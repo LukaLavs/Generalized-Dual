0e22207eabae3171d2121b7040700b2374518d91
Luka Lavš <lavsluka@gmail.com>
2025-08-17 12:38:54 +0200

    modified:   setup.py 	modified:   tests/test_2.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name="gen_dual",
     version="0.1.0",
     author="Luka Lavš",
     author_email="lavslukal@gmail.com", 
-    description="An implementation of generalized dual numbers with support for arbitrary percision and vectorization.",
-    long_description=open("README.md").read(),
+    description="An implementation of generalized dual numbers with support for arbitrary precision and vectorization.",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     keywords="automatic differentiation, generalized dual numbers, numpy, mpmath",
     url="https://github.com/LukaLavs/Automatic-Differentiation",
